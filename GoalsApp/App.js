9b6a8dca60dbf47dd07a3a7e18273d66a62209bb
Nitesh Saxena <nitesh.saxena03@gmail.com>
2024-02-22 04:06:50 +0530

    App is created and working in mobile

---
+++ GoalsApp/App.js
@@ -0,0 +1,24 @@
+import { StatusBar } from 'expo-status-bar';
+import { Button, StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <Text>This is my first practice app</Text>
+      <View>
+        <Button title='First practice button'/>
+      </View>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
