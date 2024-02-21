3755f07975cd83ea7a8a5732cb94d24b749bd578
Nitesh Saxena <nitesh.saxena03@gmail.com>
2024-02-22 04:29:30 +0530

    Creating layout using View which works same as <div>

--- GoalsApp/App.js
+++ GoalsApp/App.js
@@ -1,15 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text>This is my first practice app</Text>
       <View>
-        <Button title='First practice button'/>
+        <TextInput placeholder="Write your goal"/>
+        <Button title="Add Goal"/>
       </View>
-      <StatusBar style="auto" />
+      <View>
+        <Text>List of all the goals</Text>
+      </View>
     </View>
   );
 }
@@ -17,8 +18,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
