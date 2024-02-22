8214e0803fff7830cd83562e629dd9c7e94d32d5
Nitesh Saxena <nitesh.saxena03@gmail.com>
2024-02-23 04:09:39 +0530

    added some basic event handling to button by using usestate

--- GoalsApp/App.js
+++ GoalsApp/App.js
@@ -1,12 +1,25 @@
 import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
 import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
+  const [enteredGoalText, setEnteredGoalText] = useState('');
+
+  function goalInputHandler(enteredText) {
+    setEnteredGoalText(enteredText);
+  }
+  function addGoalHandler() {
+    console.log(enteredGoalText);
+  }
   return (
     <View style={styles.appContainer}>
       <View style={styles.inputContainer}>
-        <TextInput style={styles.textInput} placeholder="Write your goal" />
-        <Button title="Add Goal" />
+        <TextInput
+          style={styles.textInput}
+          placeholder="Write your goal"
+          onChangeText={goalInputHandler}
+        />
+        <Button title="Add Goal" onPress={addGoalHandler} />
       </View>
       <View style={styles.goalsContainer}>
         <Text>List of all the goals</Text>
@@ -17,18 +30,18 @@ export default function App() {
 
 const styles = StyleSheet.create({
   appContainer: {
-    flex:1,
+    flex: 1,
     paddingTop: 50,
     paddingHorizontal: 16,
   },
   inputContainer: {
-    flex:1,
+    flex: 1,
     flexDirection: "row",
     justifyContent: "space-between",
-    alignItems:'center',
-    marginBottom:24,
-    borderBottomWidth:5,
-    borderBottomColor:'green'
+    alignItems: "center",
+    marginBottom: 24,
+    borderBottomWidth: 5,
+    borderBottomColor: "green",
   },
   textInput: {
     borderWidth: 1,
@@ -37,7 +50,7 @@ const styles = StyleSheet.create({
     marginRight: 8,
     padding: 8,
   },
-  goalsContainer:{
-    flex:5,
-  }
+  goalsContainer: {
+    flex: 5,
+  },
 });
