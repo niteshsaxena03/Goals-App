92e0bf8176a0215cd5ad21a64704faf7cefd6b53
Nitesh Saxena <nitesh.saxena03@gmail.com>
2024-02-27 03:45:11 +0530

    adding a modal screen functionality and also a button to toggle it

--- GoalsApp/App.js
+++ GoalsApp/App.js
@@ -1,20 +1,17 @@
 import { StatusBar } from "expo-status-bar";
 import { useState } from "react";
-import {
-  Button,
-  FlatList,
-  ScrollView,
-  StyleSheet,
-  Text,
-  TextInput,
-  View,
-} from "react-native";
+import { Button, FlatList, StyleSheet, View } from "react-native";
 
 import GoalItem from "./components/GoalItem";
 import GoalInput from "./components/GoalInput";
 
 export default function App() {
+  const [modalIsVisible, setModalIsVisible] = useState(false);
   const [courseGoals, setCourseGoals] = useState([]);
+
+  function startAddGoalHandler() {
+    setModalIsVisible(true);
+  }
 
   function addGoalHandler(enteredGoalText) {
     setCourseGoals((currentCourseGoals) => [
@@ -30,7 +27,12 @@ export default function App() {
   }
   return (
     <View style={styles.appContainer}>
-      <GoalInput onAddGoal={addGoalHandler} />
+      <Button
+        title="Add new Goal"
+        color="#5e0acc"
+        onPress={startAddGoalHandler}
+      />
+      <GoalInput visible={modalIsVisible} onAddGoal={addGoalHandler} />
       <View style={styles.goalsContainer}>
         <FlatList
           data={courseGoals}
@@ -38,8 +40,8 @@ export default function App() {
             return (
               <GoalItem
                 text={itemData.item.text}
-                onDeleteItem={deleteGoalHandler}//passing this function to GoalItem.js
-                id={itemData.item.id}//this id will be passed to GoalInput.js and it will come back as a prop
+                onDeleteItem={deleteGoalHandler} //passing this function to GoalItem.js
+                id={itemData.item.id} //this id will be passed to GoalInput.js and it will come back as a prop
               />
             );
           }}
--- GoalsApp/components/GoalInput.js
+++ GoalsApp/components/GoalInput.js
@@ -1,4 +1,4 @@
-import { StyleSheet, View, TextInput, Button } from "react-native";
+import { StyleSheet, View, TextInput, Button, Modal } from "react-native";
 import { useState } from "react";
 
 function GoalInput(props) {
@@ -13,15 +13,17 @@ function GoalInput(props) {
     setEnteredGoalText(""); //clear the string after the function executes
   }
   return (
-    <View style={styles.inputContainer}>
-      <TextInput
-        style={styles.textInput}
-        placeholder="Write your goal"
-        onChangeText={goalInputHandler}
-        value={enteredGoalText}
-      />
-      <Button title="Add Goal" onPress={addGoalHandler} />
-    </View>
+    <Modal visible={props.visible} animationType="slide ">
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.textInput}
+          placeholder="Write your goal"
+          onChangeText={goalInputHandler}
+          value={enteredGoalText}
+        />
+        <Button title="Add Goal" onPress={addGoalHandler} />
+      </View>
+    </Modal>
   );
 }
 
